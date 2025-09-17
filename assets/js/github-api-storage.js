8eecbb36a721f5c0c8851a3f40d708b73a3ac86c
Antonio Contino <continocoding@gmail.com>
2025-09-17 16:40:14 +0200

    provato a customizzare lo storage

--- assets/js/github-api-storage.js
+++ assets/js/github-api-storage.js
@@ -5,189 +5,97 @@
  * bypassando la cache aggressiva di raw.githubusercontent.com.
  */
 window.githubApiStorage = {
-  repoName : null,
-  repoUser : null,
-  repoBranch : "gh-pages",
-  dataFile : "data.json",
-  getRepoInfo : function(endpoint) {
-    var result = {};
-    var parser = document.createElement('a');
-    parser.href = endpoint;
+  // --- Proprietà del driver ---
+  repo: null,
+  github: null,
+  key: null,
+  
+  // Informazioni del repository (verranno lette dal tag <script>)
+  repoUser: null,
+  repoName: null,
+  repoBranch: 'gh-pages',
+  dataFile: 'data/data.json',
 
-    var pathInfo;
-    pathInfo = parser.pathname.split("/");
-    if (parser.pathname.indexOf("/") === 0) {
-      pathInfo.shift();
+  /**
+   * Inizializzazione: legge la configurazione dal tag <script>
+   */
+  init: function(endpoint) {
+    console.log('Custom Storage [githubApiStorage]: Inizializzazione...');
+    
+    this.repoUser = document.querySelector('[data-simply-repo-user]').getAttribute('data-simply-repo-user');
+    this.repoName = document.querySelector('[data-simply-repo-name]').getAttribute('data-simply-repo-name');
+    const branch = document.querySelector('[data-simply-repo-branch]');
+    if (branch) {
+      this.repoBranch = branch.getAttribute('data-simply-repo-branch');
     }
 
-    if (parser.hostname == "github.com") {
-      result.repoUser = pathInfo.shift();
-      result.repoName =  pathInfo.shift();
-      result.repoBranch = "master";
-    } else {
-      //github.io;
-      result.repoUser = parser.hostname.split(".")[0];
-      result.repoName = pathInfo.shift();
-      result.repoBranch = "gh-pages";
+    // Carica la libreria GitHub se non è già presente
+    if (typeof Github === 'undefined') {
+        const script = document.createElement("SCRIPT");
+        // Assicurati che il percorso sia corretto per il tuo sito
+        script.src = "/Tesi/simplyedit/github.js"; 
+        document.head.appendChild(script);
     }
 
-    if (document.querySelector("[data-simply-repo-branch]")) {
-      result.repoBranch = document.querySelector("[data-simply-repo-branch]").getAttribute("data-simply-repo-branch");
-    }
-    if (document.querySelector("[data-simply-repo-name]")) {
-      result.repoName = document.querySelector("[data-simply-repo-name]").getAttribute("data-simply-repo-name");
-    }
-    if (document.querySelector("[data-simply-repo-user]")) {
-      result.repoUser = document.querySelector("[data-simply-repo-user]").getAttribute("data-simply-repo-user");
-    }
-
-    var repoPath = pathInfo.join("/");
-    repoPath = repoPath.replace(/\/$/, '');
-
-    result.repoPath = repoPath;
-    return result;
+    console.log(`Custom Storage [githubApiStorage]: Repo configurato su ${this.repoUser}/${this.repoName}, branch ${this.repoBranch}`);
   },
-  checkJail : function(url) {
-    var repo1 = this.getRepoInfo(url);
-    var repo2 = this.getRepoInfo(this.endpoint);
 
-    if (
-      (repo1.repoUser == repo2.repoUser) && 
-      (repo1.repoName == repo2.repoName) &&
-      (repo1.repoBranch == repo2.repoBranch)
-    ) {
-      return true;
-    }
-    return false;
-  },
-  init : function(endpoint) {
-    if (endpoint === null) {
-      endpoint = document.location.href.replace(document.location.hash, "");
-    }
-    var script = document.createElement("SCRIPT");
-    script.src = editor.baseURLClean + "github.js";
-    document.head.appendChild(script);
-
-    var repoInfo = this.getRepoInfo(endpoint);
-    this.repoUser = repoInfo.repoUser;
-    this.repoName = repoInfo.repoName;
-    this.repoBranch = repoInfo.repoBranch;
-
-    this.endpoint = endpoint;
-    this.dataFile = "data.json";
-    this.dataEndpoint = endpoint + "data.json";
-
-    this.sitemap = storage.default.sitemap;
-    this.listSitemap = storage.default.listSitemap;
-    this.page = storage.default.page;
-    this.escape = storage.default.escape;
-
-    if (editor.responsiveImages) {
-      if (
-        editor.settings['simply-image'] &&
-        editor.settings['simply-image'].responsive
-      ) {
-        if (typeof editor.settings['simply-image'].responsive.sizes === "function") {
-          editor.responsiveImages.sizes = editor.settings['simply-image'].responsive.sizes;
-        } else if (typeof editor.settings['simply-image'].responsive.sizes === "object") {
-          editor.responsiveImages.sizes = (function(sizes) {
-            return function(src) {
-              var result = {};
-              var info = src.split(".");
-              var extension = info.pop().toLowerCase();
-              if (extension === "jpg" || extension === "jpeg" || extension === "png") {
-                for (var i=0; i<sizes.length; i++) {
-                  result[sizes[i] + "w"] = info.join(".") + "-simply-scaled-" + sizes[i] + "." + extension;
-                }
-              }
-              return result;
-            };
-          }(editor.settings['simply-image'].responsive.sizes));
-        }
-      }
-      window.addEventListener("resize", editor.responsiveImages.resizeHandler);
-    }
-  },
-  connect : function(callback) {
-    if (typeof Github === "undefined") {
-      return false;
+  /**
+   * Connessione: gestisce l'autenticazione tramite token
+   */
+  connect: function(callback) {
+    console.log('Custom Storage [githubApiStorage]: Tentativo di connessione...');
+    
+    if (typeof Github === 'undefined') {
+        setTimeout(() => this.connect(callback), 100);
+        return;
     }
 
-    if (!editor.storage.key) {
-      editor.storage.key = localStorage.storageKey;
+    if (!this.key) {
+      this.key = localStorage.getItem('githubToken');
     }
-    if (!editor.storage.key) {
-      editor.storage.key = prompt("Please enter your authentication key");
+    if (!this.key) {
+      this.key = prompt('Please enter your GitHub Personal Access Token');
     }
 
-    if (editor.storage.validateKey(editor.storage.key)) {
-      if (!this.repo) {
-        localStorage.storageKey = editor.storage.key;
-        this.github = new Github({
-          token: editor.storage.key,
-          auth: "oauth"
-        });
-        this.repo = this.github.getRepo(this.repoUser, this.repoName);
-      }
-      if (typeof callback === "function") {
-        callback();
-      }
+    if (this.key) {
+      localStorage.setItem('githubToken', this.key);
+      this.github = new Github({
+        token: this.key,
+        auth: "oauth"
+      });
+      this.repo = this.github.getRepo(this.repoUser, this.repoName);
+      console.log('Custom Storage [githubApiStorage]: Connesso!');
+      if (callback) callback();
       return true;
     } else {
-      return editor.storage.connect(callback);
+        alert("Token di autenticazione per GitHub non fornito.");
+        return false;
     }
   },
-  disconnect : function(callback) {
-    delete this.repo;
-    delete localStorage.storageKey;
-    if (typeof callback === "function") {
-      callback();
-    }
-    return true;
-  },
-  validateKey : function(key) {
-    return true;
-  },
-  file : {
-    save : function(path, data, callback) {
-      if (path.match(/\/$/)) {
-        // github will create directories as needed.
-        var saveResult = {path : path, response: "Saved."};
-        return callback(saveResult);
-      }
 
-      var saveCallback = function(err) {
-        if (err === null) {
-          var saveResult = {path : path, response: "Saved."};
-          return callback(saveResult);
+  /**
+   * Salvataggio: scrive il file data.json tramite l'API
+   */
+  save: function(data, callback) {
+    console.log('Custom Storage [githubApiStorage]: Salvataggio dati via API...');
+    if (!this.repo) {
+        console.error("Salvataggio fallito: nessuna connessione al repository GitHub.");
+        if (callback) callback({error: true, message: "Not connected to GitHub"});
+        return;
+    }
+
+    this.repo.write(this.repoBranch, this.dataFile, data, 'SimplyEdit: Aggiornamento contenuti', (err) => {
+        if (err) {
+            console.error('Custom Storage [githubApiStorage]: Errore durante il salvataggio:', err);
+            if (callback) callback({error: true, message: err});
+        } else {
+            console.log('Custom Storage [githubApiStorage]: Dati salvati con successo.');
+            if (callback) callback({});
         }
-
-        if (err.error == 401) {
-          return callback({message : "Authorization failed.", error: true});
-        }
-        return callback({message : "SAVE FAILED: Could not store.", error: true});
-      };
-
-      var executeSave = function(path, data) {
-        editor.storage.repo.write(editor.storage.repoBranch, path, data, "Simply edit changes on " + new Date().toUTCString(), saveCallback);
-      };
-      if (data instanceof File) {
-        var fileReader = new FileReader();
-        fileReader.onload = function(evt) {
-          executeSave(path, this.result);
-        };
-        fileReader.readAsBinaryString(data);
-      } else {
-        executeSave(path, data);
-      }
-    },
-    delete : function(path, callback) {
-      editor.storage.repo.delete(editor.storage.repoBranch, path, callback);
-    }
+    });
   },
-  save : function(data, callback) {
-    return editor.storage.file.save("data.json", data, callback);
-  },
+
   /**
    * Caricamento: legge il file data.json tramite l'API per bypassare la cache
    */
@@ -195,103 +103,55 @@ window.githubApiStorage = {
     console.log('Custom Storage [githubApiStorage]: Caricamento dati via API...');
 
     const apiUrl = `https://api.github.com/repos/${this.repoUser}/${this.repoName}/contents/${this.dataFile}?ref=${this.repoBranch}`;
-
-    // Aggiungiamo un parametro per il cache-busting, che aiuta con le API
     const cacheBustUrl = apiUrl + '&t=' + new Date().getTime();
 
-    fetch(cacheBustUrl)
-      .then(response => {
-        if (response.status === 404) {
-          // Il file non esiste, è il primo avvio o il repo è vuoto
-          console.log('Custom Storage [githubApiStorage]: data.json non trovato (404). Restituisco dati vuoti.');
-          return Promise.resolve(null);
-        }
-        if (!response.ok) {
-          throw new Error(`Errore di rete dall'API GitHub: ${response.statusText}`);
-        }
-        return response.json();
-      })
-      .then(data => {
-        if (!data) {
-          // Gestisce il caso 404
-          callback('{}');
-          return;
-        }
-        // Il contenuto del file dall'API è codificato in Base64
-        const content = atob(data.content);
-        console.log('Custom Storage [githubApiStorage]: Dati caricati e decodificati con successo.');
-        callback(content);
-      })
-      .catch(error => {
-        console.error('Custom Storage [githubApiStorage]: Errore durante il caricamento via API:', error);
-        // In caso di errore, restituisce un oggetto JSON vuoto per non bloccare l'editor
+    fetch(cacheBustUrl, {
+      headers: {
+        // L'autenticazione non è necessaria per leggere un file pubblico,
+        // ma può aiutare con i rate limit dell'API.
+        // Se il repo è privato, questa parte è FONDAMENTALE.
+        ...(this.key && { 'Authorization': `token ${this.key}` })
+      }
+    })
+    .then(response => {
+      if (response.status === 404) {
+        console.log('Custom Storage [githubApiStorage]: data.json non trovato (404). Restituisco dati vuoti.');
+        return null;
+      }
+      if (!response.ok) {
+        throw new Error(`Errore di rete dall'API GitHub: ${response.statusText}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      if (!data || !data.content) {
         callback('{}');
-      });
-  },
-  saveTemplate : function(pageTemplate, callback) {
-    var dataPath = location.pathname.split(/\//, 3)[2];
-    if (dataPath.match(/\/$/)) {
-      dataPath += "index.html";
-    }
-
-    var repo = this.repo;
-    repo.read(this.repoBranch, pageTemplate, function(err, data) {
-      if (data) {
-        repo.write(this.repoBranch, dataPath, data, pageTemplate + " (copy)", callback);
+        return;
       }
+      const content = atob(data.content);
+      console.log('Custom Storage [githubApiStorage]: Dati caricati e decodificati con successo.');
+      callback(content);
+    })
+    .catch(error => {
+      console.error('Custom Storage [githubApiStorage]: Errore durante il caricamento via API:', error);
+      callback('{}');
     });
   },
-  list : function(url, callback) {
-    if (url.indexOf(editor.storage.dataEndpoint) === 0) {
-      return this.listSitemap(url, callback);
-    }
 
-    var repoInfo = this.getRepoInfo(url);
+  /**
+   * Disconnessione: pulisce il token salvato
+   */
+  disconnect: function(callback) { 
+    localStorage.removeItem('githubToken');
+    this.key = null;
+    this.github = null;
+    this.repo = null;
+    console.log('Custom Storage [githubApiStorage]: Disconnesso.');
+    if (callback) callback(); 
+  },
 
-    var repoUser = repoInfo.repoUser;
-    var repoName = repoInfo.repoName;
-    var repoBranch = repoInfo.repoBranch;
-    var repoPath = repoInfo.repoPath;
-
-    var github = new Github({});
-    var repo = github.getRepo(repoUser, repoName);
-    repo.read(repoBranch, repoPath, function(err, data) {
-      var result = {
-        images : [],
-        folders : [],
-        files : []
-      };
-
-      if (data) {
-        data = JSON.parse(data);
-        for (var i=0; i<data.length; i++) {
-          if (data[i].type == "file") {
-            var fileData = {
-              url : url + data[i].name,
-              src : url + data[i].name,
-              name : data[i].name // data[i].download_url
-            };
-            if (url === editor.storage.endpoint && data[i].name === "data.json") {
-              fileData.name = "My pages";
-              result.folders.push(fileData);
-            } else {
-              result.files.push(fileData);
-              if (fileData.url.match(/(jpg|jpeg|gif|png|bmp|tif|svg)$/i)) {
-                result.images.push(fileData);
-              }
-            }
-          } else if (data[i].type == "dir") {
-            result.folders.push({
-              url : editor.storage.endpoint + data[i].path + "/",
-              name : data[i].name
-            });
-          }
-        }
-        callback(result);
-      } else {
-        // Empty (non-existant) directory - return the empty resultset, github will create the dir automatically when we save things to it.");
-        callback(result);
-      }
-    });
+  // Funzione per validare la chiave (semplice, ritorna sempre true)
+  validateKey: function(key) {
+    return !!key;
   }
 };
