5c3db6983905de68a1368d55f6cc71dde38f4d46
Antonio Contino <continocoding@gmail.com>
2025-09-19 17:19:00 +0200

    modifica save template custom

--- simplyedit/js/simply-edit.js
+++ simplyedit/js/simply-edit.js
@@ -3302,7 +3302,7 @@
 						if (data) {
 							// Rimuovi lo slash iniziale per la chiamata API
 							var finalPath = dataPath.startsWith('/') ? dataPath.substring(1) : dataPath;
-							repo.write(branch, finalPath, data, "Create page from " + pageTemplate, function(writeErr) {
+							repo.write(this.repoBranch, finalPath, data, "Create page from " + pageTemplate, function(writeErr) {
 								if (writeErr) {
 									alert('ERRORE: La scrittura del file su GitHub è fallita. Controlla	la console per i dettagli.');
 									console.error('Errore durante repo.write:', writeErr);
