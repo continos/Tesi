39a2af6b64b84fbc9309bbba5115b5664a8e2b6c
Antonio Contino <67561554+continos@users.noreply.github.com>
2025-09-20 13:18:36 +0200

    Update simply-edit.js

--- simplyedit/js/simply-edit.js
+++ simplyedit/js/simply-edit.js
@@ -3335,7 +3335,7 @@
 					const repoPrefix = '/' + this.repoName; // Es: /Tesi
 
 					if (githubPath.startsWith(repoPrefix + '/')) {
-						githubPath = githubPath.substring(repoPrefix.length + 1); // Rimuove /Tesi/ -> research/nuova-pagina.html
+						githubPath = githubPath.substring(repoPrefix.length + 1); // Rimuove /Tesi/ da Tesi/research/nuova-pagina.html
 					}
 					console.log(`Creo pagina ${dataPath} usando il template ${pageTemplate}`);
 
