61d1767fd0a32c7bd2e8da62f4bbeef5d1d8b6f2
Antonio Contino <continocoding@gmail.com>
2025-10-07 15:14:52 +0200

    prova caso d'uso vocbench

--- simplyedit/js/simply-edit.js
+++ simplyedit/js/simply-edit.js
@@ -3557,7 +3557,7 @@
 								const newCommitSha = commitData.commit.sha;
 								bodyEl.innerHTML = `Commit ${newCommitSha.substring(0)} creato! <br> Avvio del deploy su Github Pages...<br> Verifico lo stato...`;
 								const pollDeploy = () => {							
-									const apiUrl = `https://api.github.com/repos/${repoUser}/${repoName}/pages`;
+									const apiUrl = `https://api.github.com/repos/${repoUser}/${repoName}/deployments`;
 									fetch(apiUrl, { headers: { 'Accept': 'application/vnd.github.v3+json'}}
 										.then(res => res.json())
 										.then(pagesInfo => {
